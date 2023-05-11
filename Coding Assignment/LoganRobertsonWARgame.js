fc4a2e8fa6d31d55f374688b9215164508de81af
logan robertson <loganrobertson@logans-iMac.lan>
2023-05-10 19:44:05 -0500

    2nd commit - meeting with Kristina

--- Coding Assignment/LoganRobertsonWARgame.js
+++ Coding Assignment/LoganRobertsonWARgame.js
@@ -14,7 +14,7 @@
 
 const suit = ["Spades", "Diamonds", "Hearts", "Clubs"];
 
-const value = [2,3,4,5,6,7,8,9,"J","Q","K","A"];
+const value = [2,3,4,5,6,7,8,9, 10,"J","Q","K","A"];
 
 
 //card class
@@ -22,18 +22,22 @@ const value = [2,3,4,5,6,7,8,9,"J","Q","K","A"];
 
 class Card{
     constructor(suit, value){
-        this.vaule = value;
+        this.value = value;
         this.suit = suit;
+        //need new prop. to rep. face value (jack = 11)
     }
     describeNewCard(){
-        `${this.value} of ${this.suit}`;
+       console.log(`${this.value} of ${this.suit}`);
     }
 }
 
-let card = Card.describeNewCard;
+let card = new Card(); 
 console.log(card);
+
+card.describeNewCard();
+
 //Not sure if this is the right direction, can classes pull elements from arrays?
-//should teh array be inside the class instead? Currently console.loging Undefined.
+//should the array be inside the class instead? Currently console.loging Undefined.
 
 
 
@@ -52,11 +56,35 @@ class Deck{
             this.deck.push(card);
         }
     }
+    createDeck(){
+        for(let i = 0; i < value.length; i++){
+            console.log(value[i]);
+            for(let x = 0; x < suit.length; x++){
+                console.log(suit[x]);
+                this.deck.push(new Card(suit[x], value[i])); //when pushing new card object think about string card needing values
+            }
+        }
+        console.log(this.deck[0].value); //consider when comparing cards
+    }
 }
+
+let deck = new Deck();
+deck.createDeck();
 
 //shuffle deck?
 
 //player class?
 
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+}
+
 //game logic class
 
+//deal deck evenly between 2 players
+//each deck should be an array that the order randomly shuffles
+//compare the 1st card of each to one another & assign a winner
+//counts wins for each player?
+//compare number of wins for each player to determine final winner
